57a007d543a7df12c27fa1779db6c461d3eadbc0
Prashant Cholachagudda <prchol@microsoft.com>
2017-10-18 02:00:10 +0530

    add the command to get project stats

--- legos_command.py
+++ legos_command.py
@@ -37,6 +37,22 @@ def list_issues(ctx, repo, status):
             localtiem = arrow.get(issue.created_at).to('local').humanize()
             click.echo("{:4d} {:55.55} {}".format(issue.number, issue.title, localtiem))
 
+@cli.command(name='stats')
+@click.option('--repo', metavar='<github_repo>', help='get stats for the repo')
+@click.pass_context
+def project_stats(ctx, repo):
+    '''shows the basic project stats like stars, issues, watch and PRs for tracked/single repo'''
+    print "\nProject stats"
+
+    if repo is not None:
+        print_stats(repo)
+        return
+
+    repo_list = get_tracked_repos()
+    for project in repo_list:
+        print_stats(project)
+
+
 @cli.command(name='track')
 @click.option('--repo', metavar='<github_repo>', help='add the repo to the trac list')
 @click.option('--list', is_flag=True, help='Lists all tracked repos')
@@ -70,6 +86,18 @@ def get_tracked_repos():
     list_repo = map(lambda p: p.rstrip(), file.readlines())
     file.close()
     return list_repo
+
+def print_stats(project):
+    gh = Github()
+    repo = gh.get_repo(project)
+    click.secho(repo.name, bold=True)
+    click.echo(repo.description)
+    pr_count = 0
+    for _ in repo.get_pulls(state='open'):
+        pr_count = pr_count + 1
+
+    stats_text = "stars: {0}\tissues: {1}\twatch: {2}\tPRs: {3}\n".format(repo.stargazers_count, repo.open_issues_count, repo.watchers_count, pr_count)
+    click.echo(stats_text)
 
 if  __name__ == '__main__':
     cli(obj={})
