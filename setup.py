89ca864fef39776cc3637f7b6c2cb12187442f9c
Prashant Cholachagudda <prchol@microsoft.com>
2017-10-25 22:18:31 +0530

    update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup
 setup(
     name='legos',
     version='1.0',
+    description='command line tool to fetch details from github projects/nuget packages',
+    author='Prashant Cholachagudda',
+    author_email='pvc@outlook.com',
     py_modules=['legos_command'],
     install_requires=['Click','PyGithub','arrow', 'tabulate'],
     entry_points='''
